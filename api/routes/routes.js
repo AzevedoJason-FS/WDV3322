e11cdee7696f827aad35e231350cf8ab97f52342
Jason Azevedo <JCAzevedo@student.fullsail.edu>
2022-07-10 11:03:13 -0400

    Password hash prac

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bcrypt = require("bcrypt");
 const { Mongoose } = require('mongoose');
 const router = express.Router();
 const Users = require("../model/user");
+
+const user = {}
 
 router.get("/profile", (req,res,next) => {
  res.json({
@@ -10,15 +13,37 @@ router.get("/profile", (req,res,next) => {
 });
 
 router.post("/login", (req,res,next) => {
-    res.json({
-        message:"/login - POST"
-     });
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if(err) return res.status(501).json({message: err.message});
+
+        if(result){
+            res.status(200).json({
+                message: "Authorization Successful",
+                result: result,
+            });
+        }
+            else{
+                res.status(401).json({
+                    message: "Authorization Failed",
+                    result: result,
+                });
+            }
+    })
 });
 
 router.post("/signup", (req,res,next) => {
-    res.json({
-        message:"/signup - POST"
-     });
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if(err){
+            res.status(500).json({
+                message: err.message
+            })
+        }else{
+            user.password = hash;
+            res.status(200).json({
+                password: hash
+            })
+        }
+    })
 });
 
 
